7a8b245d86e2c9418142dd9dc2e3554719b414a5
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-08-20 21:10:24 +0100

    Refactored keymaps into separate file
    - Includes clipboard setup for the work server
    - Other minor updates

--- init.lua
+++ init.lua
@@ -6,17 +6,23 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 vim.g.autoformat_enabled = false
 
+-- Netrw Config
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = -28
+
 -- ############# Lazy.nvim ############## --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -30,10 +36,23 @@ vim.g.gruvbox_material_better_performance = 1
 vim.opt.spell = false
 vim.opt.wrap = true
 vim.opt.background = "dark"
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.ruler = true
 vim.opt.showmatch = true
-vim.opt.scrolloff = 12
+vim.opt.scrolloff = 8
+
+-- Code folding using Treesitter
+vim.opt.foldenable = false
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+-- vim.opt.foldlevelstart = 1
+
+-- EOL fix
+vim.opt.fixeol = false
 
 -- Line number
 vim.opt.number = true
@@ -48,10 +67,10 @@ vim.opt.relativenumber = true
 
 vim.opt.fileformats = "dos,unix"
 
-vim.cmd([[colorscheme gruvbox-material]])
+vim.cmd.colorscheme("gruvbox-material")
 
-vim.opt.mouse = "a"               -- allow mouse usage
-vim.opt.showmode = false          -- hide mode since it is in status line
+vim.opt.mouse = "a" -- allow mouse usage
+vim.opt.showmode = false -- hide mode since it is in status line
 
 vim.opt.clipboard = "unnamedplus" -- sync system and Neovim clipboard
 
@@ -79,43 +98,27 @@ vim.opt.splitbelow = true
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
--- Handle EOL and no EOL
--- If the file already has noeol set, we want to leave it as it is
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-  desc = "Handle EOL automatically depending on previous setting",
-  callback = utils.handle_eol,
-})
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-kmap("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-kmap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-kmap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-kmap("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-kmap("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- LSP setup
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason-lspconfig").setup_handlers({
-  function(server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-    })
-  end,
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
+	end,
 })
 
 -- Session loading (on enter and on exit)
@@ -124,99 +127,70 @@ local session_name = require("utils").get_session_name
 
 -- Auto-load sesison for current directory or branch (depending on where we"re at)
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Only load the session if nvim was started with no args
-    if vim.fn.argc(-1) == 0 then
-      resession.load(session_name(), { dir = "dirsession", silence_errors = true })
-    end
-  end,
+	callback = function()
+		-- Only load the session if nvim was started with no args
+		if vim.fn.argc(-1) == 0 then
+			resession.load(session_name(), { dir = "dirsession", silence_errors = true })
+		end
+	end,
 })
 
 -- Autosave on exit
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    resession.save(session_name(), { dir = "dirsession", notify = false })
-  end,
+	callback = function()
+		resession.save(session_name(), { dir = "dirsession", notify = false })
+	end,
 })
+
+-- Vim clipboard OSC52 integration
+-- I don't have xclip here, so I'm using the OSC52 integration
+if os.getenv "SSH_CLIENT" ~= nil or os.getenv "SSH_TTY" ~= nil then
+    local function handle_paste(_)
+        return function(_)
+            local content = vim.fn.getreg '"'
+            return vim.split(content, "\n")
+        end
+    end
+
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+            ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+        },
+        paste = {
+            ["+"] = handle_paste "+",
+            ["*"] = handle_paste "*",
+        },
+    }
+end
 
 -- ########### Keymaps ########### --
 --
 -- In the future, it"ll be nice to refactor these into their own tables and files
 
-local function set_eol()
-  utils.set_eol({ save = true })
+-- Load keymaps
+local keymaps_definition = require("keymaps")
+for mode, keymaps in pairs(keymaps_definition) do
+	for key, opts in pairs(keymaps) do
+		local binding_opts = {}
+
+		if opts["desc"] ~= nil then
+			binding_opts["desc"] = opts["desc"]
+		end
+
+		if opts["noremap"] ~= nil then
+			binding_opts["noremap"] = opts["noremap"]
+		end
+
+		if opts["expr"] ~= nil then
+			binding_opts["expr"] = opts["expr"]
+		end
+
+		if opts["silent"] ~= nil then
+			binding_opts["silent"] = opts["silent"]
+		end
+
+		kmap(mode, key, opts[1], binding_opts)
+	end
 end
--- Misc
-kmap("n", "<leader>w", "<cmd>:w<CR>", { desc = "Save buffer" })
-kmap("n", "<leader>Q", "<cmd>:qa<CR>", { desc = "Exit and close all tabs" })
-kmap("n", "<leader>m", "<cmd>:marks<CR>", { desc = "Show all marks" })
-kmap("n", "<leader>eo", set_eol, { desc = "Disable and fix DOS EOL manually" })
-
--- Move Lines
-kmap("x", "J", ":move '>+1<cr>gv-gv", { desc = "Move selected line(s) down" })
-kmap("x", "K", ":move '<-2<cr>gv-gv", { desc = "Move selected line(s) down" })
-
--- Panes
-kmap("n", "<C-\\>", "<cmd>:vsplit<CR>", { desc = "Split vertically" })
-kmap("n", "<C-_>", "<cmd>:split<CR>", { desc = "Split horizontally" })
-
--- Tabs
-kmap("n", "<leader>t", "<cmd>:tabnew<CR>", { desc = "Create tab after current tab" })
-kmap("n", "<leader>T", "<cmd>:-tabnew<CR>", { desc = "Create tab before current tab" })
-kmap("n", "<leader>c", "<cmd>:tabclose<CR>", { desc = "Close current tab" })
-
--- Tab navigation
-kmap("n", "L", "<cmd>:tabnext<CR>", { desc = "Move to next tab" })
-kmap("n", "H", "<cmd>:tabprevious<CR>", { desc = "Move to previous tab" })
-kmap("n", "<leader>]", "<cmd>:+tabmove<CR>", { desc = "Move current tab to the right" })
-kmap("n", "<leader>[", "<cmd>:-tabmove<CR>", { desc = "Move current tab to the left" })
-
--- Trouble
-kmap("n", "<leader>xx", "<cmd>:TroubleToggle<CR>", { desc = "Toggle trouble" })
-kmap("n", "<leader>xr", "<cmd>:TroubleRefresh<CR>", { desc = "Refresh trouble" })
-
--- Code "actions" and LSP stuff
-kmap("v", "<leader>/", "gc", { noremap = false, desc = "Toggle commenting current selection" })
-kmap("n", "<leader>/", "gcc", { noremap = false, desc = "Toggle commenting line under cursor" })
-
-kmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, desc = "Go to definition" })
-kmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, desc = "Go to definition" })
-kmap("n", "gt", "<cmd>tabnew | vim.lsp.buf.definition()<CR>", { noremap = true, desc = "Go to definition in new tab" })
-
-kmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, desc = "Show diagnostic info" })
-kmap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, desc = "Go to implementation" })
-kmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, desc = "Show references" })
-kmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, desc = "Hover to show documentation" })
-
--- Jumplist mapping
-kmap("n", "go", "<C-o>", { noremap = true, desc = "Jump to previous location" })
-kmap("n", "gi", "<C-i>", { noremap = true, desc = "Jump to next location" })
-
-kmap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", { noremap = true, desc = "Show code action menu" })
-kmap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", {
-  noremap = true,
-  desc = "Format current buffer",
-})
-kmap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { noremap = true, desc = "Rename symbol" })
-kmap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, desc = "Show signature help" })
-kmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", { noremap = true, desc = "Go to next diagnosis" })
-kmap("n", "]D", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", {
-  noremap = true,
-  desc = "Go to previous diagnosis",
-})
-
--- Disabled on purpose
--- kmap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", { noremap = true, desc = "Format buffer" })
-
--- Session mappings
-kmap("n", "<leader>sf", resession.list, { desc = "List all available saved sessions" })
-kmap("n", "<leader>ss", resession.save, { desc = "Save session for current directory" })
-kmap("n", "<leader>sa", resession.save_all, { desc = "Save all sessions" })
-kmap("n", "<leader>st", resession.save_tab, { desc = "Save session for current tab" })
-kmap("n", "<leader>s.", resession.load, { desc = "Load last session for current directory" })
-kmap("n", "<leader>sd", resession.delete, { desc = "Select and delete session" })
-
--- Git stuff
-kmap("n", "<leader>gb", "<cmd>GitBlameToggle<CR>", { desc = "Toggle Git blame" })
-kmap("n", "<leader>gc", "<cmd>GitBlameCopyCommitURL<CR>", { desc = "Copy commit URL" })
-kmap("n", "<leader>gC", "<cmd>GitBlameCopyFileURL<CR>", { desc = "Copy file upstream URL" })
---
+++ lua/keymaps.lua
@@ -0,0 +1,175 @@
+local utils = require("utils")
+
+local resession = require("resession")
+local builtin = require("telescope.builtin")
+
+return {
+	-- Normal mode
+	n = {
+		["<Esc>"] = { "<cmd>nohlsearch<CR>" },
+
+		-- Keybinds to make split navigation easier.
+		--  Use CTRL+<hjkl> to switch between windows
+		--
+		--  See `:help wincmd` for a list of all window commands
+		["<C-h>"] = { "<C-w>h", desc = "Move focus to the left window" },
+		["<C-l>"] = { "<C-w>l", desc = "Move focus to the right window" },
+		["<C-j>"] = { "<C-w><C-j>", desc = "Move focus to the lower window" },
+		["<C-k>"] = { "<C-w><C-k>", desc = "Move focus to the upper window" },
+
+		-- Misc useful maps
+		["<leader>Q"] = { "<cmd>:qa<CR>", desc = "Exit and close all tabs" },
+		["<leader>w"] = { "<cmd>:w<CR>", desc = "Save buffer" },
+		["<leader>m"] = { "<cmd>:marks<CR>", desc = "Show all marks" },
+		["<leader>eo"] = {
+			function()
+				utils.set_eol({ save = true })
+			end,
+			desc = "Disable and fix DOS EOL manually",
+		},
+		["<leader>lH"] = {
+			function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+			end,
+			desc = "Toggle inlay hints",
+		},
+
+		-- Copilot
+		["<leader>cc"] = {
+			function()
+				require("copilot.suggestion").toggle_auto_trigger()
+			end,
+			desc = "Toggle copilot suggestion",
+		},
+
+		-- Spell check
+		["<leader>sp"] = { utils.toggle_spellcheck, desc = "Toggle spellcheck" },
+		["<leader>sz"] = { "[s1z=``", desc = "Fix spelling errors" },
+
+		-- Movements
+		-- Move between wrapped lines
+		["k"] = {
+			function()
+				return vim.v.count == 0 and "gk" or "k"
+			end,
+			expr = true,
+			noremap = true,
+		},
+		["j"] = {
+			function()
+				return vim.v.count == 0 and "gj" or "j"
+			end,
+			expr = true,
+			noremap = true,
+		},
+
+		-- Panes
+		["<C-\\>"] = { "<cmd>:vsplit<CR>", desc = "Split vertically" },
+		["<C-_>"] = { "<cmd>:split<CR>", desc = "Split horizontally" },
+
+		-- Tabs
+		["<leader>t"] = { "<cmd>:tabnew<CR>", desc = "Create tab after current tab" },
+		["<leader>T"] = { "<cmd>:-tabnew<CR>", desc = "Create tab before current tab" },
+		["<leader>c"] = { "<cmd>:tabclose<CR>", desc = "Close current tab" },
+
+		-- Tab navigation
+		["L"] = { "<cmd>:tabnext<CR>", desc = "Move to next tab" },
+		["H"] = { "<cmd>:tabprevious<CR>", desc = "Move to previous tab" },
+		["<leader>]"] = { "<cmd>:+tabmove<CR>", desc = "Move current tab to the right" },
+		["<leader>["] = { "<cmd>:-tabmove<CR>", desc = "Move current tab to the left" },
+
+		-- Trouble
+		["<leader>x"] = { "", desc = "Trouble" },
+		["<leader>xx"] = { "<cmd>Trouble diagnostics toggle focus=true<cr>", desc = "Diagnostics (Trouble)" },
+		["<leader>xb"] = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics (trouble)" },
+		["<leader>xs"] = {
+			"<cmd>Trouble symbols toggle pinned=true results.win.relative=win results.win.position=right<cr>",
+			desc = "Symbols (trouble)",
+		},
+		["<leader>xL"] = {
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		["<leader>xk"] = { "<cmd>Trouble diagnostics prev<cr>", desc = "Previous item" },
+		["<leader>xj"] = { "<cmd>Trouble diagnostics next<cr>", desc = "Next item" },
+		["<leader>xl"] = { "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+		["<leader>xq"] = { "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+
+		-- Code "actions" and LSP stuff
+		["<leader>/"] = { "gcc", noremap = false, desc = "Toggle commenting line under cursor" },
+
+		["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", noremap = true, desc = "Go to definition" },
+		["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>", noremap = true, desc = "Go to definition" },
+		["gt"] = { "<cmd>tabnew | vim.lsp.buf.definition()<CR>", noremap = true, desc = "Go to definition in new tab" },
+
+		["gl"] = { "<cmd>lua vim.diagnostic.open_float()<CR>", noremap = true, desc = "Show diagnostic info" },
+		["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", noremap = true, desc = "Go to implementation" },
+		["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", noremap = true, desc = "Show references" },
+		["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", noremap = true, desc = "Hover to show documentation" },
+
+		["<leader>l"] = { "", desc = "LSP" },
+		["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", noremap = true, desc = "Show code action menu" },
+		["<leader>lf"] = {
+			"<cmd>lua vim.lsp.buf.format( async = true })<CR>",
+			noremap = true,
+			desc = "Format current buffer",
+		},
+		["<leader>lr"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", noremap = true, desc = "Rename symbol" },
+		["<leader>ls"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", noremap = true, desc = "Show signature help" },
+		["]d"] = { "<cmd>lua vim.diagnostic.goto_next(buffer=0})<cr>", noremap = true, desc = "Go to next diagnosis" },
+		["]D"] = {
+			"<cmd>lua vim.diagnostic.goto_prev(buffer=0})<cr>",
+			noremap = true,
+			desc = "Go to previous diagnosis",
+		},
+
+		-- Jumplist mapping
+		["go"] = { "<C-o>", noremap = true, desc = "Jump to previous location" },
+		["gi"] = { "<C-i>", noremap = true, desc = "Jump to next location" },
+
+		-- Session mappings
+		["<leader>s"] = { "", desc = "Session" },
+		["<leader>sf"] = { resession.list, desc = "List all available saved sessions" },
+		["<leader>ss"] = { resession.save, desc = "Save session for current directory" },
+		["<leader>sa"] = { resession.save_all, desc = "Save all sessions" },
+		["<leader>st"] = { resession.save_tab, desc = "Save session for current tab" },
+		["<leader>s."] = { resession.load, desc = "Load last session for current directory" },
+		["<leader>sd"] = { resession.delete, desc = "Select and delete session" },
+
+		-- Git stuff
+		["<leader>g"] = { "", desc = "Git" },
+		["<leader>gb"] = { "<cmd>GitBlameToggle<CR>", desc = "Toggle Git blame" },
+		["<leader>gy"] = { "<cmd>GitBlameCopyCommitURL<CR>", desc = "Copy commit URL" },
+		["<leader>gY"] = { "<cmd>GitBlameCopyFileURL<CR>", desc = "Copy file upstream URL" },
+
+		-- Conflicts
+		["<leader>gc"] = { "<cmd>GitConflictListQf<CR>", desc = "Show a quickfix list of conflicts" },
+
+		["<leader>gcb"] = { "<cmd>GitConflictChooseBoth<CR>", desc = "Accept both changes" },
+		["<leader>gco"] = { "<cmd>GitConflictChooseOurs<CR>", desc = "Select our changes" },
+		["<leader>gct"] = { "<cmd>GitConflictChooseTheirs<CR>", desc = "Accept incoming changes" },
+		["<leader>gcn"] = { "<cmd>GitConflictChooseNone<CR>", desc = "Select none of the changes" },
+
+		["<leader>g]"] = { "<cmd>GitConflictNextConflict<CR>", desc = "Go to next conflict" },
+		["<leader>g["] = { "<cmd>GitConflictPrevConflict<CR>", desc = "Go to previous conflict" },
+
+		-- Telescope bindings
+		["<leader>f"] = { "", desc = "Telescope" },
+		["<leader>ff"] = { builtin.find_files, desc = "Search files" },
+		["<leader>fw"] = { builtin.grep_string, desc = "Search current word" },
+		["<leader>fg"] = { builtin.live_grep, desc = "Live grep in current directory/workspace" },
+	},
+
+	-- Select mode
+	v = {
+		["<leader>/"] = { "gc", noremap = false, desc = "Toggle commenting current selection" },
+	},
+
+	-- Visual mode
+	x = {
+		-- Movements
+		-- Move lines
+		["J"] = { ":move '>+1<cr>gv-gv", desc = "Move selected line(s) down" },
+		["K"] = { ":move '<-2<cr>gv-gv", desc = "Move selected line(s) down" },
+	},
+}
